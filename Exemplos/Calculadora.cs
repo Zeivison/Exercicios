f5386d87979be7b17998a549dc1b5a8211eeaba2
Zeivison <deivisonalcantaracunha@gmail.com>
2026-08-18 19:27:58 -0300

    diretorio fixacao

---
+++ Exemplos/Calculadora.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Exemplos
+{
+    internal class Calculadora
+    {
+        public int Somar(int x, int y)
+        {
+            return x + y;
+        }
+
+        public static int Subtrair(int x, int y)
+        {
+            return x - y;
+        }
+
+
+    }
+}
--- Exemplos/Program.cs
+++ Exemplos/Program.cs
@@ -1,1 +1,15 @@
-﻿Console.WriteLine("Hello, World!");
+﻿using Exemplos;
+
+Console.Write("Informe sua idade: ");
+int idade = int.Parse(Console.ReadLine());
+
+Console.Write("Digite seu nome: ");
+string nome = Console.ReadLine();
+
+Console.WriteLine($"Idade = {idade}");
+Console.WriteLine($"O nome informado foi {nome}");
+
+Calculadora calc = new Calculadora();
+
+int resultado = calc.Somar(22, 20);
+Console.WriteLine($"O resultado da soma é: {resultado}");
